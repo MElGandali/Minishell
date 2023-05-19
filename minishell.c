a003e8ddc30661b81642e35804d0fa1ec3ad1432
Mohssen El Gandali <mel-gand@c2r6p2.1337.ma>
2023-05-19 15:10:23 +0100

    all splitted

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: mel-gand <mel-gand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 12:33:42 by mel-gand          #+#    #+#             */
-/*   Updated: 2023/05/12 15:17:33 by mel-gand         ###   ########.fr       */
+/*   Updated: 2023/05/19 12:58:34 by mel-gand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int main()
     while (1)
     {
         lex.line = readline("bash$ ");
+        add_history(lex.line);
         tokenizer(&lex);
     }
     return (0); 
--- split_pipe_redir.c
+++ split_pipe_redir.c
@@ -6,7 +6,7 @@
 /*   By: mel-gand <mel-gand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 11:42:22 by maddou            #+#    #+#             */
-/*   Updated: 2023/05/18 23:23:20 by mel-gand         ###   ########.fr       */
+/*   Updated: 2023/05/19 15:05:45 by mel-gand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,169 +14,124 @@
 
 int check_token(t_lexer *lex, int i)
 {
-    if (ft_strnstr(lex->word[i], "|") == 0 ||ft_strnstr(lex->word[i], "<<") == 0
-        || ft_strnstr(lex->word[i], ">>") == 0 || ft_strnstr(lex->word[i], "<") == 0
-        || ft_strnstr(lex->word[i], ">") == 0 || ft_strnstr(lex->word[i], ">|") == 0   
-        || ft_strnstr(lex->word[i], "|>") == 0 || ft_strnstr(lex->word[i], "|<") == 0)
-            return (0);
-    return (1);
+	if (ft_strnstr(lex->word[i], "|") == 0 ||ft_strnstr(lex->word[i], "<<") == 0
+		|| ft_strnstr(lex->word[i], ">>") == 0 || ft_strnstr(lex->word[i], "<") == 0
+		|| ft_strnstr(lex->word[i], ">") == 0 || ft_strnstr(lex->word[i], ">|") == 0   
+		|| ft_strnstr(lex->word[i], "|>") == 0 || ft_strnstr(lex->word[i], "|<") == 0)
+			return (0);
+	return (1);
 }
 
 void count_tokens (t_lexer *lex)
 {
-    int i;
-    int j;
+	int i;
+	int j;
 
-    i = 0;
-    lex->word_nb = 0;
-    while(lex->word[i])
-    {
-        if (check_token(lex, i) == 0)
-        {
-                j = 0;
-                while (lex->word[i][j])
-                {
-                    if ((lex->word[i][j] == '\'' || lex->word[i][j] == '\"') || (!(lex->word[i][j] == '>' || lex->word[i][j] == '<' || lex->word[i][j] == '|')))
-                    {
-                         while(!(lex->word[i][j] == '>' || lex->word[i][j] == '<' 
-                            || lex->word[i][j] == '|') && lex->word[i][j] != '\0')
-                            {
-                                if (lex->word[i][j] == '\'' || lex->word[i][j] == '\"')
-                                {
-                                    j = skip_quote(lex->word[i], j);
-                                }
-                                    j++;
-                                // else if ((!(lex->word[i][j] == '>' || lex->word[i][j] == '<' || lex->word[i][j] == '|' 
-                                // || lex->word[i][j] == '\'' || lex->word[i][j] == '\"' )) && lex->word[i][j] != '\0')
-                                //     j++;
-                            }
-                        lex->word_nb++;
-                    }
-                    if ((lex->word[i][j] == '>' || lex->word[i][j] == '<' || lex->word[i][j] == '|') && lex->word[i][j] != '\0')
-                    {
-                        while((lex->word[i][j] == '>' || lex->word[i][j] == '<' 
-                            || lex->word[i][j] == '|') && lex->word[i][j] != '\0')
-                            j++;
-                        lex->word_nb++;
-                    }
-                }
-        }
-        else
-            lex->word_nb++;
-        i++;
-        }
-    }
-
-
-
-
-
-
-    
-//     while(lex->word[i])
-//     {
-//         printf("check %d\n", check_token(lex, i));
-//         if (check_token(lex, i) == 0)
-//         {
-//             while (lex->word[i][j])
-//             {
-//                 if (lex->word[i][j] == '|' || lex->word[i][j] == '>' || lex->word[i][j] == '<')
-//                 {
-//                     lex->word_nb++;
-//                     while ((lex->word[i][j] == '|' || lex->word[i][j] == '>' 
-//                     || lex->word[i][j] == '<') && lex->word[i][j + 1])
-//                         j++;
-//                 }
-//                 else if (lex->word[i][j] != '|' || lex->word[i][j] != '>' || lex->word[i][j] != '<')
-//                 {
-//                     while ((lex->word[i][j] != '|' || lex->word[i][j] != '>' 
-//                     || lex->word[i][j] != '<') && lex->word[i][j + 1])
-//                     {
-//                         if (lex->word[i][j] == '\"' || lex->word[i][j] == '\'')
-//                             j = skip_quote(lex->word[i], j) - 1;
-//                         j++;
-//                     }
-//                     lex->word_nb++;
-//                 }
-//                 j++;
-//             } 
-//         }
-//         i++;
-//     }
-// }
+	i = 0;
+	lex->word_nb = 0;
+	while(lex->word[i])
+	{
+		if (check_token(lex, i) == 0)
+		{
+				j = 0;
+				while (lex->word[i][j])
+				{
+					if ((lex->word[i][j] == '\'' || lex->word[i][j] == '\"') || (!(lex->word[i][j] == '>' 
+						|| lex->word[i][j] == '<' || lex->word[i][j] == '|')))
+					{
+						 while(!(lex->word[i][j] == '>' || lex->word[i][j] == '<' 
+							|| lex->word[i][j] == '|') && lex->word[i][j] != '\0')
+							{
+								if (lex->word[i][j] == '\'' || lex->word[i][j] == '\"')
+									j = skip_quote(lex->word[i], j);
+								j++;
+							}
+						lex->word_nb++;
+					}
+					if ((lex->word[i][j] == '>' || lex->word[i][j] == '<' || lex->word[i][j] == '|') 
+						&& lex->word[i][j] != '\0')
+					{
+						while((lex->word[i][j] == '>' || lex->word[i][j] == '<' 
+							|| lex->word[i][j] == '|') && lex->word[i][j] != '\0')
+							j++;
+						lex->word_nb++;
+					}
+				}
+		}
+		else
+			lex->word_nb++;
+		i++;
+		}
+	}
 
 int    find_word(t_lexer *lex, int i, int j)
 {
-    if (lex->word[i][j] == '|' || lex->word[i][j] == '>' || lex->word[i][j] == '<')
-    {
-        while ((lex->word[i][j + 1] == '|' || lex->word[i][j + 1] == '>' 
-        || lex->word[i][j + 1] == '<') && lex->word[i][j + 1]) // if we change the || to && we will find that it split every redirection seperately even if they're next to each other
-            j++;
-        lex->end = j;
-    }
-    else if (lex->word[i][j] != '|' || lex->word[i][j] != '>' || lex->word[i][j] != '<')
-    {
-        while ((lex->word[i][j] != '|' && lex->word[i][j] != '>' 
-        && lex->word[i][j] != '<') && lex->word[i][j + 1]) //"qqdwqdq'" |'|' >> '|qdqwd' "|"         'qw'     //ok|>ok this test will work if we do j + 1 here -> lex->word[i][j + 1]
-        {
-            if (lex->word[i][j] == '\"' || lex->word[i][j] == '\'')
-                j = skip_quote(lex->word[i], j); 
-            // if (lex->word[i][j + 1] == '|' || lex->word[i][j + 1] == '>' 
-            //     || lex->word[i][j + 1] == '<') // "hello| " '|'       "\"|he;lo
-            //     j--;
-            if(lex->word[i][j + 1] == '|' || lex->word[i][j + 1] == '>' || lex->word[i][j + 1] == '<')
-                    break;
-            j++;
-            
-        }
-        lex->end = j;
-    }//<< qwdqd'qwdq"' q <"qwdq"qd           '      '
-    return (j);
+	if (lex->word[i][j] == '|' || lex->word[i][j] == '>' || lex->word[i][j] == '<')
+	{
+		while ((lex->word[i][j + 1] == '|' || lex->word[i][j + 1] == '>' 
+		|| lex->word[i][j + 1] == '<') && lex->word[i][j + 1]) // if we change the || to && we will find that it split every redirection seperately even if they're next to each other
+			j++;
+		lex->end = j;
+	}
+	else if (lex->word[i][j] != '|' || lex->word[i][j] != '>' || lex->word[i][j] != '<')
+	{
+		while ((lex->word[i][j] != '|' && lex->word[i][j] != '>' 
+		&& lex->word[i][j] != '<') && lex->word[i][j + 1]) //"qqdwqdq'" |'|' >> '|qdqwd' "|"         'qw'     //ok|>ok this test will work if we do j + 1 here -> lex->word[i][j + 1]
+		{
+			if (lex->word[i][j] == '\"' || lex->word[i][j] == '\'')
+				j = skip_quote(lex->word[i], j);
+			 // "hello| " '|'       "\"|he;lo
+			if(lex->word[i][j + 1] == '|' || lex->word[i][j + 1] == '>' || lex->word[i][j + 1] == '<')
+					break;
+			j++;   
+		}
+		lex->end = j;
+	}//<< qwdqd'qwdq"' q <"qwdq"qd           '      '
+	return (j);
 }
 
 int split_pipe_redir(t_lexer *lex)
 {
-    int i;
-    int j;
-    int k;
+	int i;
+	int j;
+	int k;
 
-    i = 0;
-    j = 0;
-    k = 0;
-    count_tokens(lex);
-        lex->token = (char **)malloc(sizeof(char *) * (lex->word_nb + 1));
-        if (!lex->token)
-            return (0);
-        while (lex->word[i] != NULL)
-        {
-            j = 0;
-            if (ft_strnstr(lex->word[i], ">") == 0 || ft_strnstr(lex->word[i], "<") == 0 
-                || ft_strnstr(lex->word[i], "|") == 0)
-            {
-                while(lex->word[i][j] != '\0')
-                {
-                    lex->start = j;
-                    printf("start%d\n", lex->start);
-                    j = find_word(lex, i, j);
-                    printf("end%d\n", lex->end);
-                    lex->token[k++] = ft_substr(lex->word[i], lex->start, lex->end - lex->start);
-                    j++;
-                }
-            }
-            else
-                lex->token[k++] = ft_substr(lex->word[i], 0, ft_strlen(lex->word[i]) - 1);
-            i++;
-        }
-        lex->token[k] = NULL;
-
-        printf("%d\n", lex->word_nb);
-        i = 0;
-        while (lex->token[i])
-        {
-            
-            printf("%s\n", lex->token[i]);
-            i++;
-        }
-    return (1);
+	i = 0;
+	j = 0;
+	k = 0;
+	count_tokens(lex);
+	lex->token = (char **)malloc(sizeof(char *) * (lex->word_nb + 1));
+	if (!lex->token)
+		return (0);
+	while (lex->word[i] != NULL)
+	{
+		j = 0;
+		if (ft_strnstr(lex->word[i], ">") == 0
+			|| ft_strnstr(lex->word[i], "<") == 0
+			|| ft_strnstr(lex->word[i], "|") == 0)
+		{
+			while(lex->word[i][j] != '\0')
+			{
+				lex->start = j;
+				j = find_word(lex, i, j);
+				lex->token[k++] = ft_substr(lex->word[i], lex->start, lex->end - lex->start);
+				j++;
+			}
+		}
+		else
+			lex->token[k++] = ft_substr(lex->word[i], 0, ft_strlen(lex->word[i]) - 1);
+		i++;
+	}
+	lex->token[k] = NULL;
+	
+		// printf("%d\n", lex->word_nb);
+		// i = 0;
+		// while (lex->token[i])
+		// {
+			
+		// 	printf("%s\n", lex->token[i]);
+		// 	i++;
+		// }
+	return (1);
 }
 
