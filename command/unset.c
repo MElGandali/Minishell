9f8c6357bed413b430ae35dc91baba24abe2ce31
Marouane Addou <maddou@c2r6p10.1337.ma>
2023-07-10 12:20:45 +0100

    fixed an error in the unset command

--- command/unset.c
+++ command/unset.c
@@ -6,7 +6,7 @@
 /*   By: maddou <maddou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 15:36:48 by maddou            #+#    #+#             */
-/*   Updated: 2023/07/09 18:14:52 by maddou           ###   ########.fr       */
+/*   Updated: 2023/07/10 12:09:02 by maddou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,23 +25,23 @@ void unset_command(t_parser *parser)
         j = length(parser->lex->exp);
         u = 0;
         v = 1;
-        if (check_valid_key(parser->comm[i].new_cmd[j]) == 0)
+        while (parser->comm[u].new_cmd[v] != NULL)
         {
-            while (parser->comm[u].new_cmd[v] != NULL)
+            if (check_valid_key(parser->comm[u].new_cmd[v]) == 0)
             {
                 if (i != 0)
                     remove_node(&parser->lex->env, parser->comm[u].new_cmd[v]);
                 if (j != 0)
                     remove_node(&parser->lex->exp, parser->comm[u].new_cmd[v]);
                 g_exit = 0;
-                v++;
             }
+            else  
+            {
+                g_exit = 1;
+                ft_printf ("bash: unset: `%s': not a valid identifier\n", parser->comm[u].new_cmd[v]);
+            }
+            v++;
         }
-        // else  
-        // {
-        //     g_exit = 1;
-        //         ft_printf ("bash: unset: `%s': not a valid identifier\n", parser->comm[i].new_cmd[j]);
-        // }
     }
-    
 }
+  
